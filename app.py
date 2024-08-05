cc137e8a4e3fa13b47c1f29eaadf10423890ac72
Nilesh Agarwal <nick.aggarwal@gmail.com>
2024-08-06 01:15:09 +0530

    Create app.py

---
+++ app.py
@@ -0,0 +1,20 @@
+from transformers import AutoTokenizer, AutoModel
+import torch
+
+class InferlessPythonModel:
+    def initialize(self):
+        self.tokenizer = AutoTokenizer.from_pretrained('BAAI/bge-large-zh-v1.5')
+        self.model = AutoModel.from_pretrained('BAAI/bge-large-zh-v1.5')
+        self.model.eval()
+
+    def infer(self, inputs):
+        sentences = inputs["sentences"]
+        encoded_input = self.tokenizer(sentences, padding=True, truncation=True, return_tensors='pt')
+        with torch.no_grad():
+            model_output = self.model(**encoded_input)
+            sentence_embeddings = model_output[0][:, 0]
+        sentence_embeddings = torch.nn.functional.normalize(sentence_embeddings, p=2, dim=1)
+        return {'sentence_embeddings': sentence_embeddings.tolist()}
+
+    def finalize(self):
+        pass
