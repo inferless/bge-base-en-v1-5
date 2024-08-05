53674e7e5eaaae54804a1a06f7720779515432a3
Rajdeep Borgohain <150957746+rbgo404@users.noreply.github.com>
2024-08-06 00:23:16 +0530

    Create input_schema.py

---
+++ input_schema.py
@@ -0,0 +1,8 @@
+INPUT_SCHEMA = {
+    "sentences": {
+        'datatype': 'STRING',
+        'required': True,
+        'shape': [-1],
+        'example': ["F1-score is calculated based on token matching with the ground truth answer.","It measures the overlap between the model's generated answer and the correct answer at the token level."]
+    }
+}
